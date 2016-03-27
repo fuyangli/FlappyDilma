7927b22ecaf62cbb07049b33dadb89b140aff800
Gabriel Fernandes Lopes Guaitolini <guaitolinigabriel@gmail.com>
2016-03-27 18:04:26 -0300

    Audio para Android

--- www/js/game.js
+++ www/js/game.js
@@ -13,8 +13,8 @@ var mainState = {
 
     // Load the jump sound
     game.load.audio('jump', 'assets/jump.wav');
-    game.load.audio('cachorro', 'assets/cachorro.wav');
-    game.load.audio('dentifricio', 'assets/dentifricio.wav');
+    game.load.audio('cachorro', ['assets/cachorro.ogg', 'assets/cachorro.mp3']);
+    game.load.audio('dentifricio', ['assets/dentifricio.ogg', 'assets/dentifricio.mp3']);
 
 
     game.load.image('jumpbutton', 'assets/jump.png');
@@ -32,8 +32,12 @@ var mainState = {
 
     this.pipes = game.add.group();
     this.pipes.enableBody = true;
-    this.pipes.createMultiple(20, 'pipe');
-    //this.pipes.scale.setTo(0.3,0.3);
+    this.pipes.createMultiple(30, 'pipe');
+
+    this.scoreCollider = this.game.add.group();
+    this.scoreCollider.enableBody = true;
+    this.scoreCollider.createMultiple(30, 'bird');
+
     this.timer = this.game.time.events.loop(1500, this.addRowOfPipes, this);
 
     this.bird = this.game.add.sprite(100, 245, 'bird');
@@ -43,6 +47,8 @@ var mainState = {
 
     // New anchor position
     this.bird.anchor.setTo(-0.2, 0.5);
+
+  
 
     game.input.onDown.add(this.jump, this);
 
@@ -57,20 +63,29 @@ var mainState = {
   },
 
   update: function() {
-    if (this.bird.inWorld == false)
+    if (this.bird.inWorld == false){
       this.restartGame();
+    }
 
     game.physics.arcade.overlap(this.bird, this.pipes, this.hitPipe, null, this);
-
+    game.physics.arcade.overlap(this.bird, this.scoreCollider, this.hitScore, null, this);
     // Slowly rotate the bird downward, up to a certain point.
-    if (this.bird.angle < 20)
+    if (this.bird.angle < 20){
       this.bird.angle += 1;
+    }
+  },
+  render : function() {
+    game.debug.body(this.bird);
+    this.pipes.forEachAlive(function(pipe) {
+      game.debug.body(pipe);
+    }, this)
   },
 
   jump: function() {
     // If the bird is dead, he can't jump
-    if (this.bird.alive == false)
+    if (this.bird.alive == false){
       return;
+    }
 
     this.bird.body.velocity.y = -350;
 
@@ -79,14 +94,16 @@ var mainState = {
       angle: -20
     }, 100).start();
 
+
     // Play sound
     this.jumpsSound[Math.floor(Math.random() * this.jumpsSound.length) + 0].play();
   },
 
   hitPipe: function() {
     // If the bird has already hit a pipe, we have nothing to do
-    if (this.bird.alive == false)
+    if (this.bird.alive == false){
       return;
+    }
 
     // Set the alive property of the bird to false
     this.bird.alive = false;
@@ -98,48 +115,44 @@ var mainState = {
     this.pipes.forEachAlive(function(p) {
       p.body.velocity.x = 0;
     }, this);
+    this.scoreCollider.forEachAlive(function(p) {
+      p.body.velocity.x = 0;
+    }, this);
   },
-
+  hitScore: function(player, scoreable) {
+    scoreable.destroy();
+    this.score += 1;
+    this.labelScore.setText(this.score);
+  },
   restartGame: function() {
     game.state.start('main');
   },
-
+  addOneScoreCollider: function(x,y) {
+    var col = this.scoreCollider.getFirstDead();
+    col.reset(x, y);
+    col.scale.setTo(0.10, 0.10);
+    col.body.velocity.x = -200;
+    col.checkWorldBounds = true;
+    col.outOfBoundsKill = true;
+  },
   addOnePipe: function(x, y) {
     var pipe = this.pipes.getFirstDead();
-
     pipe.reset(x, y);
     pipe.scale.setTo(0.25, 0.25);
     pipe.body.velocity.x = -200;
     pipe.checkWorldBounds = true;
     pipe.outOfBoundsKill = true;
   },
-
   addRowOfPipes: function() {
     var hole = Math.floor(Math.random() * 5) + 1;
-
-    for (var i = 0; i < 8; i++)
-      if (i != hole && i != hole + 1)
+    this.addOneScoreCollider(this.width,  (((hole) * 60  + 10) + ((hole + 1) * 60  + 10)) / 2);
+    for (var i = 0; i < 9; i++) {
+      if (i != hole && i != hole + 1) {
         this.addOnePipe(this.width, i * 60 + 10);
-
-    this.score += 1;
-    this.labelScore.text = this.score;
+      }
+    }
   },
 };
-
-function resizeGame() {
-  var height = $('body').height();
-  var width = $('body').width();
-  game.width = width;
-  game.height = height;
-  game.stage._bounds.width = width;
-  game.stage._bounds.height = height;
-  if (game.renderType === Phaser.WEBGL) {
-    game.renderer.resize(width, height);
-  }
-}
-// $(window).resize(function() {
-//   resizeGame();
-// });
 
 game.state.add('main', mainState);
 game.state.start('main');
